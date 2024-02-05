6de639d454b114b26b2a103b587ad7f6c337712f
NellsRelo <nellsrelo@gmail.com>
2024-02-05 13:06:30 -0800

    Initial Flag Dump

---
+++ CommunityLibrary/Mods/CommunityLibrary/ScriptExtender/Lua/IdDictionary/StaticData/Flags/Flags.lua
@@ -0,0 +1,37 @@
+Globals.Flags = {}
+Globals.Flags.Class = {
+  Paladin = {
+    Ancients = {
+      BrokeOath = "7cf0bd9c-f089-45a3-88fb-03087d3d8b95"
+    }
+  }
+}
+
+Globals.Flags.Companions = {
+  LeavePartyStayPresent = "5f7db208-df3c-8cf8-a397-1d188ed79523"
+}
+
+Globals.Flags.Crime = {
+  AgreedToForgive = "5b39abcc-3266-9ced-de99-e28ed8255542",
+  CharismaDisabled = "4bed5f63-dbf9-4fb2-be84-c29edf10a96a",
+  CrimeWarning2 = "4e017c74-10c1-b90f-6d61-969b1bdb8d38",
+  IsVictim = "5e9f6262-7bab-4e25-894e-32ee674208b2",
+  TrespassConvincePermanent = "7bedda15-54d7-498d-b73b-74d4f2edce16"
+}
+
+Globals.Flags.Misc = {
+  DisappearOutOfSight = "5dc33889-cbfb-c3d2-9e4e-8d826bf8566f",
+  HasAShovel = "5df98788-6881-4c28-9b91-ac77d5451bc2",
+  Impossible = "6e4b66e9-0f72-5171-04ba-441108b45b0e",
+  ItemPickup = "5a79a482-ff4d-4e5e-95a4-b972af80077d",
+  MindHasBeenRead = "7d47fd14-e9bc-4f8a-e7db-b5df2493f2cd",
+  ResurrectedWhileNotInParty = "6d11649a-f5b0-4919-b54b-e66188ddd414",
+  TransferNPCPayment2 = "6e07cd32-5bde-85c8-6c0d-18b83f465a21"
+}
+
+Globals.Flags.Origins = {
+  InclusionRandom = "5c169560-2732-c515-9e73-06ba1fd768f0",
+  InExclusiveRelationship = "6c1a31e8-1d3d-42a5-af4f-72ef7a798f74",
+  InvitedToCampRan = "07c4d37e-fc82-4055-b768-167650be9956",
+  RemoveFromPartyAfterDialog = "7a429beb-fbfb-fa8a-3a33-0349323ad11d"
+}
