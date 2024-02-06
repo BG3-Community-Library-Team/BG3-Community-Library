848ce81e1cb3b9048aa8a8efb5f6dacab236e7d9
NellsRelo <nellsrelo@gmail.com>
2024-02-05 17:22:20 -0800

    Fix Waypoint Typo, Add more Flags

--- CommunityLibrary/Mods/CommunityLibrary/ScriptExtender/Lua/IdDictionary/StaticData/Flags/Flags.lua
+++ CommunityLibrary/Mods/CommunityLibrary/ScriptExtender/Lua/IdDictionary/StaticData/Flags/Flags.lua
@@ -2,11 +2,20 @@ Globals.Flags = {}
 
 Globals.Flags.AnimalHandlingSuccess = {
   ['fdb45066-04be-cd40-ec7d-a0fc274d8604_AnimalHandlingSuccess'] = "98ae67eb-39fb-d1f4-c310-6a4f4e8320de",
-  ['babc3558-80e2-5c56-6405-cc7516034956_AnimalHandlingSuccess'] = "9615e4ec-b39d-5110-1755-180f07bceb18"
+  ['babc3558-80e2-5c56-6405-cc7516034956_AnimalHandlingSuccess'] = "9615e4ec-b39d-5110-1755-180f07bceb18",
+  ['3bebb23e-6e7d-8e0f-5a1f-0c8e5b4d3229_AnimalHandlingSuccess'] = "a47c81a6-be44-ca9c-2ed6-5b5e7924121d",
+  ['201f31b9-12f2-0a3a-54f4-77de9b6457fa_AnimalHandlingSuccess'] = "a712d787-8cac-b25a-9dcc-c2dd117b93be",
+  ['63bcf660-0d01-2ca3-571a-c2ffc9cc579b_AnimalHandlingSuccess'] = "aa9a57be-64f6-e43d-3599-d5317ae18fcd",
+  ['984ee2a0-0333-b151-46cf-9ca53724dd0d_AnimalHandlingSuccess'] = "b0c2a081-282e-55c6-9204-3a95767beae3",
+  ['40ccf18e-f84c-591d-4929-0562b34ba0ee_AnimalHandlingSuccess'] = "bb134a85-945a-2a40-3402-71c8c668c6b7",
+  ['0d8795bb-0aa9-7cf7-aac0-c6d7fe1e761d_AnimalHandlingSuccess'] = "d7e7d1ad-9648-c51a-1372-955d3451c567"
 }
 
 Globals.Flags.Camp = {
+  DBGCampString1 = "b75b6383-4e62-4ebd-f09c-b1d4f2ae21c8",
   DBGCampString3 = "1bc8a2f1-7e9b-01a5-3cf4-1dd0a1e7da7d",
+  DBGCampString4 = "a407ff83-e819-82eb-555f-4e33d871897c",
+  DBGCampString5 = "ac88446a-c38a-3544-1664-940ae46dba75",
   DBGCampString6 = "31c78931-6de6-e4b6-046a-1f975207e1c5",
   DBGCampString7 = "18a94f12-4fee-6637-44bf-af9c9fba23cb",
   DBGCampString8 = "52e5327f-93a0-3888-db77-fbc30f06041e",
@@ -15,9 +24,11 @@ Globals.Flags.Camp = {
   DBGPopulateNights = "022ded9e-3b49-9b50-d7d1-9c7069b3159e",
   DBGCampType3 = "17b577a0-9135-8215-0b3c-fbc20c1d7f29",
   DBGCampType4 = "2e4a196d-5520-0b42-5579-01eebb1ed61e",
+  DBGCampType5 = "cfcbdf4c-3651-593d-0cc2-99c7d699f6b9",
   DBGCampType6 = "31be0538-f808-220d-def3-52a5f1d5a76e",
   DBGCampType7 = "73c53af3-43a6-0bca-1cb7-4dde91bc2009",
   DBGGoToNight1 = "4a20172a-0ff4-b6bd-a19b-364f40d7b6b6",
+  DBGGoToNight3 = "bd19d762-19a5-ce3c-4995-345f97468723",
   DBGGoToNight4 = "01da48ec-a173-a600-3b11-283fd80a8cbb",
   DBGGoToNight5 = "469d499f-e29e-8e64-e43d-0131cf977214",
   DBGIgnoreCampTypeRestrictions = "4118a620-c0e5-afaf-1bec-2e260882a45f",
@@ -25,7 +36,12 @@ Globals.Flags.Camp = {
   DBGInviteAllCompanions = "98bcd2b6-c8d5-1a8e-72df-0eb0ded536ee",
   DBGInviteAllFollowers = "7492e9a8-119a-40ed-2950-911bb849924e",
   DBGInviteIndiPopulate = "41a67fb9-2eb9-d517-0b96-b364a7acc0ca",
+  DBGNoNightSelection = "b7831dfa-d67c-05aa-4abd-b12c4a36dc52",
+  AddSupplies = "d6a890c8-f55e-283d-6907-40be1865280f",
+  OriginTiredOfTalkingTo = "40225d88-6220-43ec-9427-c2ee73a3deb7",
+  OpenLedger = "bc764992-d770-a23b-68f8-d5f25e7e18bb",
   PartySizeAtLeast2 = "3bf26823-3143-4d1a-85e6-8a7e04273d77",
+  PartySizeAtLeast4 = "62130ed1-4567-4f3a-82ed-7801dc58e01a",
   SkipSleepCutscene = "1ee0a25e-4115-44ef-b87d-c2a5eee494b6",
   SetDay = "2d1a001d-2416-4b9c-b929-66b6367eceb4",
   State_InCamp = "161b7223-039d-4ebe-986f-1dcd9a66733f"
@@ -34,6 +50,8 @@ Globals.Flags.Camp = {
 Globals.Flags.Class = {
   Paladin = {
     AboutToBecomeOathbreaker = "3ad5db08-0191-4308-b127-ad1cdc6f8718",
+    AllObtainedRedemption = "a7694991-e2b0-4720-9970-748166b7c52c",
+    BecomesOathbreaker = "a5664f6e-929e-449d-9cc4-ea212d37a4b1",
     BrokeOath = "0246da96-5ef8-4c65-9f0d-c2856bd17674",
     BrokeOathAncients = "7cf0bd9c-f089-45a3-88fb-03087d3d8b95",
     CanRedemptThemself = "5918cf72-d4f6-3b5e-0a23-2bba9264a623",
@@ -44,50 +62,71 @@ Globals.Flags.Class = {
 }
 
 Globals.Flags.Companions = {
-  LeavePartyStayPresent = "5f7db208-df3c-8cf8-a397-1d188ed79523",
+  Combat = "d8f0c309-193b-eaed-5aaa-812cd137caf2",
   DisappearingOutOfSight = "2b762f9e-52b7-4ade-ad20-cc26edaa3c31",
+  FollowerDisappearOutOfSight = "98162019-f069-4f54-ac62-0c8559c3866d",
   HasBeenRecruited = "57c070c4-9dfb-4d61-9e7b-4111bc1df030",
-  CompanionMurder = "2b416989-93e2-4853-b48a-7aeed3e69f7c"
+  LeavePartyStayPresent = "5f7db208-df3c-8cf8-a397-1d188ed79523",
+  Murder = "2b416989-93e2-4853-b48a-7aeed3e69f7c"
 }
 
 Globals.Flags.Crime = {
+  ClearTag_FUGITIVE = "b7e841d0-8b8c-c18b-48af-8d2a283f62e6",
   AgreedToForgive = "5b39abcc-3266-9ced-de99-e28ed8255542",
   AllowArrest = "2677bf25-03f3-6323-c72a-0ca61fe5ad01",
+  ArrestHavePrison = "602741c9-b5be-6479-f456-4781383551ff",
   ArrestPlayer = "4434aa46-3370-47e8-8c3f-c9d9f9b7bce5",
+  Assault = "bd371f4a-4123-5b42-3dd0-9cf23fb8c955",
   BribeDisabled = "9e426938-8a06-8f2d-3d7d-7bdc975c3c02",
   BribingDisabled_NextTime = "30b21fcf-d1d7-42b3-9ea4-0af3c8f8852a",
   CharismaDisabled = "4bed5f63-dbf9-4fb2-be84-c29edf10a96a",
+  Charmed = "39293705-87ad-46c2-ae32-5ad8b492313e",
+  DropEvidence = "4139762a-96a5-06e7-7786-b1982855804f",
   DroppedOwnedCorpse = "39fd4ed5-cb85-4427-978d-cd6dc303361b",
+  GuiltFound = "c742b680-a456-82ca-f157-3cd31c099a80",
   KnockSpell = "1182b0be-111c-4480-8417-76f3129cc761",
+  Murder = "cdea5897-ce77-48d4-8377-003e453fe8ba",
   Theft = "8dd72b27-dcc4-67c3-17d0-ef0170f21d85",
   Trespassing = "8cb905dc-3480-ee1e-fbdb-03b317c6a46f",
   Vandalize = "0cf27cb8-2ad2-cbe0-cee6-85f8338e668b",
   Warning1 = "9d8c50f1-f67d-4290-bb41-9616cb482b88",
   Warning2 = "4e017c74-10c1-b90f-6d61-969b1bdb8d38",
-  IsVictim = "5e9f6262-7bab-4e25-894e-32ee674208b2",
+  Warning3 = "b4003dc1-1591-93c9-45b4-3b5d1581423f",
   IsAngry = "60a1ad4c-ab66-4ca6-973c-a9b49bb84892",
+  IsVictim = "5e9f6262-7bab-4e25-894e-32ee674208b2",
+  IsInPrison = "c9b75b21-6eba-065e-7680-fc9a0c5838e4",
   InterrogationState_Murder = "1693e3c1-5d5e-4c07-b287-62d0700b6b7b",
+  InterrogationState_Assault = "a6ae9ba4-d5fe-4ed7-9eeb-d974571f2493",
+  InterrogrationState_Theft = "ba40739c-1bc1-493e-8617-846c625496db",
   FleeHelpFromDialog = "686bc362-67ee-8d88-384a-25c737aaf740",
+  FoundEvidenceCurrentCrime = "65040012-add4-05f8-0924-9d2e27c3e7ed",
   PassedForbiddenItemUseWarning = "8aed3a6b-6d13-719f-05ed-65ebd2568ee7",
+  PersuadedGuard = "bdcb6798-b606-5c94-95d1-e03b4414c0af",
   PlayerFoundGuilty = "1b28f6ce-09ad-9664-9865-6ed4eb69a0b7",
   Polymorphed = "1243c7b6-3d96-464f-b501-9d4162ef885d",
   PersuasionFailed = "827d3b2c-f154-03a7-227a-16192c201ee2",
   ResistSearch = "542c71fa-0019-9464-ba46-00ff8d1856f3",
+  ReturnGoodsToOwner = "464633ad-7dda-f4b0-d168-844b19d6ff96",
   TrespassConvincePermanent = "7bedda15-54d7-498d-b73b-74d4f2edce16",
   ConvincedTrespassAllowedPemanently = "77fff462-8394-4733-9a40-8a4c6ef776c3",
-  UseForbiddenItem = "0203c578-3de4-5976-5494-5d5ad94bca80"
+  UseForbiddenItem = "0203c578-3de4-5976-5494-5d5ad94bca80",
+  MusicalPerformance_Tip = "79759568-32ac-472c-9fe2-590b7dd6dcd3"
 }
 
 Globals.Flags.Dialog = {
   AddNearbyPlayersToDialog = "775fb200-cfe9-0928-e7a2-73b73b2a3ab7",
   CharismaDisabledNextTime = "3e72e9cf-3199-4a9c-9e21-8e187177a952",
   CompanionLeaves = "363c71f4-8b46-c0c0-4bbb-0e5a85e4652d",
+  CrimeDialog = "c1083e9c-5ff7-4cf2-9326-5d64fe2b5a14",
   DisableRelationshipDialogsPermanently = "1b1d90ae-583f-4e91-9016-6cf6446a055a",
   FactionHostileToIndividualPlayerAfterDialog = "3a7747bd-004a-6e69-7ff5-4f4eb6c3c223",
   LeaveDisappearInstant = "8d15c1c9-fa85-4c67-8f85-c5c1bb672b28",
-  SpeakWithDeadQuestionAsked = "9984cd59-2fad-46cb-bf61-96af6034fbea",
-  SpeakWithDeadQuestionsLimitReached = "01c40268-4a8d-4787-8566-77e01ff04762",
-  SpeakWithDeadKiller = "7827d93e-1619-402e-b9ec-3ec5d762b6ea",
+  PartneredCompanionIncluded = "c401d64f-025e-433d-911d-1e32d0da37fa",
+  SetOriginHostileAfterDialog = "91659c6d-ed58-4b95-894b-763b69df824d",
+  SpeakWithDead_QuestionAsked = "9984cd59-2fad-46cb-bf61-96af6034fbea",
+  SpeakWithDead_QuestionsLimitReached = "01c40268-4a8d-4787-8566-77e01ff04762",
+  SpeakWithDead_Killer = "7827d93e-1619-402e-b9ec-3ec5d762b6ea",
+  SpeakWithDead_PartySpokeWithDead = "b375c49f-b908-4794-b12c-bf2c7122e19e",
 }
 
 Globals.Flags.Items = {
@@ -95,6 +134,7 @@ Globals.Flags.Items = {
   HasAShovel = "5df98788-6881-4c28-9b91-ac77d5451bc2",
   HasTaggedItem_FOOD = "1c762978-ba29-4260-81fd-f19c9ad1e5a4",
   HasTaggedItem_FISH = "9a83c4a4-952f-4da2-9f92-3692fbe17f10",
+  HasTaggedItem_HEALING_POTION = "a60b5bbc-4991-4957-ab17-3948aebcc154",
   HasTaggedItem_LOCKPICK = "88b02016-bc68-4618-a9b7-44c3b8ee5a8a",
   ItemPickup = "5a79a482-ff4d-4e5e-95a4-b972af80077d",
   ShotTriggerState_NotForSale = "48c7bdbc-f83a-4890-a178-200bc9e127cc",
@@ -113,45 +153,58 @@ Globals.Flags.MindHasBeenRead = {
   ['d76f6036-01e3-9b7c-4a31-45d3532ec723_MindHasBeenRead'] = "96cfd76b-0d34-0e17-e9c2-b5e32f945321",
   ['1e03e6de-12ee-7575-b98d-c1c62db40e5e_MindHasBeenRead'] = "294b255d-cdd6-e2ce-80f3-d1fecc6bf6a7",
   ['0cbba5cb-15d6-ffbf-7ccf-86d1c634e55d_MindHasBeenRead'] = "786b5682-2ff9-cc73-8701-6dfc059c6865",
-  ['0a609c31-78f5-f58d-e715-ba475b0b0ea5_MindHasBeenRead'] = "836db9c4-9454-8ec2-651f-86c4e377b4b6"
+  ['0a609c31-78f5-f58d-e715-ba475b0b0ea5_MindHasBeenRead'] = "836db9c4-9454-8ec2-651f-86c4e377b4b6",
+  ['fca8b921-9243-492c-9180-0a582da474a0_MindHasBeenRead'] = "31717f6f-0427-8c34-ea22-cd00635ccf72",
+  ['02ffed33-4f85-abba-8561-c0a5f23c5ade_MindHasBeenRead'] = "818417b3-6031-c6a4-364c-2d4fa647acce",
+  ['6c01bf72-690e-8be4-b05e-cce08840e35d_MindHasBeenRead'] = "2031576c-89e2-e21b-6210-31555f0a1207",
+  ['c00f7b18-9639-d350-c4db-00286689c32a_MindHasBeenRead'] = "2049567e-f85e-e545-f0ea-1f3b06094437",
+  ['3bb55791-3613-a6b2-a0dd-87bcd1b6d059_MindHasBeenRead'] = "15341280-fb94-e911-2d7d-4beae8765bde",
+  ['dc3a598b-8c79-8836-55c3-8e16903bffa0_MindHasBeenRead'] = "a16d5ad8-cca1-3028-5315-cc6e54a9d924",
+  ['15db943e-b4aa-15d1-0b50-6acfbc5956a8_MindHasBeenRead'] = "a382f71f-27ef-2e63-a6a7-8b8a9deb5b5f",
+  ['757073d4-2568-aa79-c309-f9ae3660e127_MindHasBeenRead'] = "a2956f3d-0b09-f3de-853f-ba01d7b342d1",
+  ['c399c0d8-1d4c-a87a-7240-1e359e366e03_MindHasBeenRead'] = "b0d01416-9667-31eb-af15-9cf6df5cdc84",
+  ['de422dde-0d5b-6f8c-abd9-ed62938f87fd_MindHasBeenRead'] = "b8756acc-4684-cc6c-bbc0-2f36d8c33d99",
+  ['db1aa315-6fe2-40b5-c206-e06a88573a5c_MindHasBeenRead'] = "be82b829-844d-6f7d-3444-b29640c6c727",
+  ['fe3994e0-b4a3-b818-0bdb-9b2dac66ffa7_MindHasBeenRead'] = "c6af337d-60e0-8f93-ce63-bec9eb74174b",
+  ['7b84d47c-d076-5eed-f762-5f49f443a4c6_MindHasBeenRead'] = "c7667ab3-07be-8cd5-b4eb-f291f78e8e42",
+  ['a9b978cd-201b-74b9-4506-351e3644e184_MindHasBeenRead'] = "d54e482f-a7cc-67df-ffab-bc915567275d"
 }
 
 Globals.Flags.Misc = {
+  DBG_BecomeAvatar = "34371b8e-a34b-3354-54e7-fcda800d703a",
   AvatarNotPresent = "286a8a58-5fbe-61d5-40cf-c299487d0a74",
   BlockChaining = "9b8c01fc-8cc8-40d5-a8df-59a725a578c9",
-  ClearKnockdown = "7dcd4aac-a388-4124-963e-d85fec572d44",
-  ClearKnockdown2 = "1edfd5fb-955e-4091-bd16-72a0ff14b26e",
+  ChangeAttitude = "a73f2d9f-ee7f-4fa2-a6ce-1f839ca48e13",
+  CharacterDie = "a0103c05-578b-4d42-b1e2-3b8ded63d0c1",
   Concealed = "1869d286-ea41-4b3e-8665-3a3a33963ff7",
   CP_WarningSomeone = "038aa41c-c4e4-4892-99de-84b7f20ff045",
   DisableReflectionsPermanently = "95a92a40-9a19-4285-a9ea-1b672047d105",
   DisappearOutOfSight = "5dc33889-cbfb-c3d2-9e4e-8d826bf8566f",
+  DisappearOutOfSightWhenAvailable = "a5b0b401-a04d-6493-579b-d87cff1474cb",
+  DontAppearAfter = "a1085866-f30e-4165-865c-9992feec580e",
   EndTurn = "78cd5286-9c0d-4dea-9033-1d6276be52e5",
   HasSelectedPreset = "2acc4437-4ba5-4205-b045-3596d66c050b",
+  FixedDChosen = "b9f7fb4d-bff7-4058-b93f-5da46627c80b",
   FixedEChosen = "95c3c9a4-71a6-473b-b243-3455cffde92d",
   Impossible = "6e4b66e9-0f72-5171-04ba-441108b45b0e",
   LeverStateBlocked = "473cad6c-a161-43ca-b1ed-8f9d4cb5ea57",
-  ReactToA = "1b1c379a-912e-d542-4e0b-5242d0fa628f",
-  ReactToB = "32027f59-385f-4abc-2f13-ca5e28dc98d9",
+  PreventTradeWithPlayer = "b4405483-6875-4b6a-80ae-e6248ead0313",
   ResurrectedWhileNotInParty = "6d11649a-f5b0-4919-b54b-e66188ddd414",
-  SetKnockdown = "01f59f7d-7cea-4822-81e5-2802cd58f4b6",
   SetupUserAlignments = "67bd759d-24aa-4b19-89ff-bd2a38c086d5",
   SkipToTurn = "9418ba09-2d39-40bc-81ec-f9d7a05310ce",
   SWA_AnimalHandlingFail = "5164cbce-060c-81cb-c6fc-79758ae8ff84",
   SWA_HasMet = "34ac840e-6ec6-5bd7-0d91-810cd9cc2c3b",
-}
-
-Globals.Flags.TransferNPCPayment = {
-  TransferNPCPayment2 = "6e07cd32-5bde-85c8-6c0d-18b83f465a21",
-  TransferNPCPayment4 = "1fde8abb-e22a-85a5-4971-4b0bae93879e",
-  TransferNPCPayment7 = "07d97ab6-4a6c-44ba-a593-77ce1498fcff"
+  SWA_HasMet_SpeakWithAnimals = "ad7c3fdf-c758-194b-5175-97565211c2f3"
 }
 
 Globals.Flags.Origins = {
   DBGSetAllTags = "3cde4737-618d-07ae-b846-3d020593fb60",
+  DBGClearAllTags = "aece0b35-4276-5e8d-3ec3-2f0eae8048b7",
   AddToParty = "4870b2cd-210c-0fdc-9c58-4d0142bdae29",
   InclusionRandom = "5c169560-2732-c515-9e73-06ba1fd768f0",
-  InclusionPIckedAtRandom2 = "32f45064-c5f2-465f-ac2d-5ad0c1ce9f59",
-  InExclusiveRelationship = "6c1a31e8-1d3d-42a5-af4f-72ef7a798f74",
+  InclusionPickedAtRandom2 = "32f45064-c5f2-465f-ac2d-5ad0c1ce9f59",
+  Inclusion2Random = "c30251ab-eab9-4ed8-bdf9-e1e816b7f12c",
+  InclusionEnd = "b5261e06-a89e-0ec8-bab9-2329ff72b6d1",
   InvitedToCampRan = "07c4d37e-fc82-4055-b768-167650be9956",
   RemoveFromPartyAfterDialog = "7a429beb-fbfb-fa8a-3a33-0349323ad11d",
   SetAllTags_ReallyOrigins = "227ba3e7-70b5-3de4-5b1d-add6a3f47101",
@@ -166,4 +219,38 @@ Globals.Flags.Player = {
 
 Globals.Flags.Quests = {
   SandboxQuest_GoBack = "6733ab6f-2993-45aa-a426-7531935075e8"
+}
+
+Globals.Flags.ReactTo = {
+  ['d630e1f6-a570-a524-31b9-d9d02deb2de6_ReactToA'] = "1b1c379a-912e-d542-4e0b-5242d0fa628f",
+  ['d630e1f6-a570-a524-31b9-d9d02deb2de6_ReactToB'] = "32027f59-385f-4abc-2f13-ca5e28dc98d9"
+}
+
+Globals.Flags.Romance = {
+  DoubleDating = "41320aeb-8e1a-433d-a82e-3d78aff578da",
+  InExclusiveRelationship = "6c1a31e8-1d3d-42a5-af4f-72ef7a798f74",
+}
+
+Globals.Flags.Status = {
+  RemovePolymorph = "ac08aa18-f4b8-4a5e-ae4e-f788811fa149",
+  SetKnockdown = "01f59f7d-7cea-4822-81e5-2802cd58f4b6",
+  SetKnockdown2 = "cb977481-185d-4848-9999-c3f52b26dbfb",
+  ClearKnockdown = "7dcd4aac-a388-4124-963e-d85fec572d44",
+  ClearKnockdown2 = "1edfd5fb-955e-4091-bd16-72a0ff14b26e",
+  Cowering = "cf16110e-3767-4585-8028-83cb9857adcd",
+  NoCower = "8180293e-4d0d-4e9b-9dfc-b195d4c67ddb"
+}
+
+Globals.Flags.TransferNPCPayment = {
+  TransferNPCPayment = "41179aa7-bc85-02b5-1399-71a651d9dc85",
+  TransferNPCPayment2 = "6e07cd32-5bde-85c8-6c0d-18b83f465a21",
+  TransferNPCPayment3 = "74658cac-b888-ddaa-a306-ffa04dc42645",
+  TransferNPCPayment4 = "1fde8abb-e22a-85a5-4971-4b0bae93879e",
+  TransferNPCPayment5 = "07487248-1537-4a10-9588-f69ec72da4b9",
+  TransferNPCPayment6 = "c639ec94-24d1-45a0-9a37-caa9b21a86de",
+  TransferNPCPayment7 = "07d97ab6-4a6c-44ba-a593-77ce1498fcff"
+}
+
+Globals.Flags.UI = {
+  OpenWaypointUI = "OpenWaypointUI"
 }
